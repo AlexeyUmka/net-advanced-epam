c1740cd7f79f8c749c35c5b5f23c7d07f931aa8b
oleksii-yurchenko-easypark <oleksii.yurchenko@easypark.net>
2023-12-05 17:52:57 +0100

    Add logging

--- layered-architectures-module-2/Carting.gRPC/Services/CartingService.cs
+++ layered-architectures-module-2/Carting.gRPC/Services/CartingService.cs
@@ -18,51 +18,73 @@ public class CartingService : Carting.CartingBase
         _mapper = mapper;
     }
 
-    public override async Task<CartResponse> GetCartByExternalIdAsync(CartByExternalIdRequest request, ServerCallContext context)
+    public override async Task<CartResponse> GetCartByExternalIdAsync(CartByExternalIdRequest request,
+        ServerCallContext context)
     {
+        _logger.LogInformation($"Getting cart by ExternalId {request.CartExternalId}");
         var result = _mapper.Map<CartResponse>(await _cartService.GetCartByExternalIdAsync(request.CartExternalId));
+        _logger.LogInformation($"Successfully retrieved cart with ExternalId {request.CartExternalId}");
         return result;
     }
 
-    public override async Task GetCartItemsByExternalId(CartByExternalIdRequest request, IServerStreamWriter<CartItemResponse> responseStream,
+    public override async Task GetCartItemsByExternalId(CartByExternalIdRequest request,
+        IServerStreamWriter<CartItemResponse> responseStream,
         ServerCallContext context)
     {
         var cart = _mapper.Map<CartResponse>(await _cartService.GetCartByExternalIdAsync(request.CartExternalId));
+        _logger.LogInformation($"Successfully retrieved items for cart with ExternalId {request.CartExternalId}");
 
         foreach (var item in cart.Items)
         {
             await responseStream.WriteAsync(item, context.CancellationToken);
+            _logger.LogInformation($"Item {item} written to response stream");
         }
     }
 
-    public override async Task<CartResponse> AddItemToCart(IAsyncStreamReader<AddCartItemRequest> requestStream, ServerCallContext context)
+    public override async Task<CartResponse> AddItemToCart(IAsyncStreamReader<AddCartItemRequest> requestStream,
+        ServerCallContext context)
     {
         var lastCartId = 0;
+
         while (await requestStream.MoveNext(context.CancellationToken))
         {
             var requestItem = requestStream.Current;
             var cartItem = MapFromAddCartItemRequest(requestItem);
+            _logger.LogInformation($"Mapping AddCartItemRequest {requestItem.ItemName}");
+
             await _cartService.AddCartItemAsync(requestItem.CartExternalId, cartItem);
+            _logger.LogInformation($"Item {requestItem.ItemName} added to cart with id {requestItem.CartExternalId}");
+
             lastCartId = requestItem.CartExternalId;
         }
-        
+
         var result = _mapper.Map<CartResponse>(await _cartService.GetCartByExternalIdAsync(lastCartId));
+        _logger.LogInformation($"Successfully added items to cart with ExternalId {lastCartId}");
+
         return result;
     }
 
-    public override async Task AddItemToCartBiDirectional(IAsyncStreamReader<AddCartItemRequest> requestStream, IServerStreamWriter<CartResponse> responseStream,
+    public override async Task AddItemToCartBiDirectional(IAsyncStreamReader<AddCartItemRequest> requestStream,
+        IServerStreamWriter<CartResponse> responseStream,
         ServerCallContext context)
     {
         while (await requestStream.MoveNext(context.CancellationToken))
         {
             var requestItem = requestStream.Current;
             var cartItem = MapFromAddCartItemRequest(requestItem);
+            _logger.LogInformation($"Mapping AddCartItemRequest {requestItem.ItemName}");
+
             await _cartService.AddCartItemAsync(requestItem.CartExternalId, cartItem);
-            var result = _mapper.Map<CartResponse>(await _cartService.GetCartByExternalIdAsync(requestItem.CartExternalId));
+            _logger.LogInformation($"Item {requestItem.ItemName} added to cart with id {requestItem.CartExternalId}");
+
+            var result =
+                _mapper.Map<CartResponse>(await _cartService.GetCartByExternalIdAsync(requestItem.CartExternalId));
             await responseStream.WriteAsync(result, context.CancellationToken);
+            _logger.LogInformation(
+                $"Successfully added items to cart with ExternalId {requestItem.CartExternalId} and written to response stream");
         }
     }
-
+    
     private CartItem MapFromAddCartItemRequest(AddCartItemRequest requestItem)
     {
         return new CartItem()
